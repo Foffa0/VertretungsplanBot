fda32ab26f064ed56643524db6f7cca2824dbf39
christopher <hoden@krebs.de>
2021-06-23 23:04:43 +0200

    change bot status

--- bot.py
+++ bot.py
@@ -98,7 +98,9 @@ async def on_ready():
     client.loop.create_task(autodelete_background_task()) #starts the background task
     while True:
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="!help")) #create custom bot state
-        
+        await asyncio.sleep(15)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="!invite me onto your server"))
+        await asyncio.sleep(15)
 
 @client.event
 async def on_message(message):
