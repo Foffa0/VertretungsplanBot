c14b33c6ce7223b9005fc20db7dd343e75ba06ac
christopher <hoden@krebs.de>
2021-07-01 15:25:53 +0200

    fix autodeletion problem

--- autodelete.py
+++ autodelete.py
@@ -15,18 +15,20 @@ async def msgAddAutodelete(message, days):
     message_id = message.id
     expiration_day = datetime.today().weekday() 
     for day_count in range(days):
-        expiration_day = expiration_day + 1 if expiration_day < 6 else 0
-    # print(f"added message for autodeleting after {days} day(s)")
-    # print(expiration_day)
+        expiration_day = expiration_day + 1 if expiration_day < 6 else -6
     messageList.append(Autodelete(message_id, message.channel.id, expiration_day))     
 
 #checks the day and deletes all messages from yesterday
 async def deleteMessages(client):
     today = datetime.today().weekday()
     for m in messageList:
+        print(m.channel_id, m.msg_id)
         if m.expiration_date == today:
-            await client.http.delete_message(m.channel_id, m.msg_id)
-    print("deleted all messages from yesterday")
+            try:
+                await client.http.delete_message(m.channel_id, m.msg_id)
+            except:
+                pass
+            messageList.remove(m)
 
 async def deleteIn(client,message,seconds=0,minutes=0,days=0):
     now = datetime.now()
